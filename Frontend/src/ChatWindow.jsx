ec96db1f1a922b73b15aac5273e7d99d6132a57f
Rohit2968 <rohit2968raj@gmail.com>
2025-07-26 12:24:27 +0530

    Added backend live link

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -26,7 +26,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("http://localhost:5000/api/chat", options);
+            const response = await fetch("https://customgpt-backend-oytb.onrender.com/api/chat", options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply);
